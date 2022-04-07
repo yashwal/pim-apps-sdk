306cd2dc528de63f45622a9e29a6131132986b74
Yashaskara <kryj999@gmail.com>
2022-04-07 20:04:38 +0530

    Bug fix

--- src/pim_apps/utils.py
+++ src/pim_apps/utils.py
@@ -6,21 +6,18 @@ os.environ['A2C_BASE_URL']="https://api.api2cart.com/"
 os.environ['PEPPERX_URL']="https://pim.unbxd.io/pepperx/"
 
 
-class GetDomains:
-    def __init__(self):
-        pass
 
-    def get_pim_app_domain(self):
-        return os.environ['PIM_APP_BASE_URL']
+def get_pim_app_domain():
+    return os.environ['PIM_APP_BASE_URL']
 
 
-    def get_pim_domain(self):
-        return os.environ['PIM_BASE_URL']
+def get_pim_domain():
+    return os.environ['PIM_BASE_URL']
 
 
-    def get_a2c_domain(self):
-        return os.environ['A2C_BASE_URL']
+def get_a2c_domain():
+    return os.environ['A2C_BASE_URL']
 
 
-    def get_pepperx_domain(self):
-        return os.environ['PEPPERX_URL']
+def get_pepperx_domain():
+    return os.environ['PEPPERX_URL']
