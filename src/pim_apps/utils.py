12494fdcdebee55b9fcf4fc603c70deb51f3fe92
Siva Narayana V <31622150+sivanv-unbxd@users.noreply.github.com>
2023-01-12 16:56:57 +0530

    Update utils.py

--- src/pim_apps/utils.py
+++ src/pim_apps/utils.py
@@ -149,9 +149,21 @@ def upload_to_s3(filename):
     :return: True if file was uploaded, else False
     """
     bucket = "unbxd-pim-ui"
-    region = os.environ.get('aws_region') or os.environ.get(f"{str('aws_region').upper()}")
-    aws_access_key_id = os.environ.get('aws_access_key_id') or os.environ.get(f"{str('aws_access_key_id').upper()}")
-    aws_secret_access_key = os.environ.get('aws_secret_access_key') or os.environ.get(f"{str('aws_secret_access_key').upper()}")
+    if 'aws_region' in os.environ:
+        region = os.environ.get('aws_region')
+    else:
+        region = os.environ.get(f"{str('aws_region').upper()}")
+
+    if 'aws_access_key_id' in os.environ:
+        aws_access_key_id = os.environ.get('aws_access_key_id')
+    else:
+        aws_access_key_id = os.environ.get(f"{str('aws_access_key_id').upper()}")
+
+    if 'aws_secret_access_key' in os.environ:
+        aws_secret_access_key = os.environ.get('aws_secret_access_key')
+    else:
+        aws_secret_access_key = os.environ.get(f"{str('aws_secret_access_key').upper()}")
+
     key = "app-uploads/" + filename
     object_name = filename
     s3 = boto3.resource(
