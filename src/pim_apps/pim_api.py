c6a3e943433ba2ad07049dbb41e626892ea35493
Yashwal S Kanchan <92976414+yashwal@users.noreply.github.com>
2024-01-17 14:33:07 +0530

    Update pim_api.py

--- src/pim_apps/pim_api.py
+++ src/pim_apps/pim_api.py
@@ -423,85 +423,101 @@ class ProductProcessor(object):
 
     
     def process_and_format_success_products(self, products_link, include_variants=False, exclude_pim_properties=False):
-        df = pd.read_json(products_link)
-
-        # If group_by_parent is FALSE, then only VARIANT and SOLO products will be sent as siblings
-        if not self.pim_channel_api.group_by_parent:
+        try:
+            df = pd.read_json(products_link)
+            
+            if df.empty:
+                # If empty, return an empty list
+                return []
+    
+            # If group_by_parent is FALSE, then only VARIANT and SOLO products will be sent as siblings
+            if not self.pim_channel_api.group_by_parent:
+                df_variant = df[df['pimProductType'] == 'VARIANT']
+                df_solo = df[df['pimProductType'] == 'SOLO']
+                
+                if exclude_pim_properties:
+                    df_variant = self.exclude_pim_properties(df_variant)
+                    df_solo = self.exclude_pim_properties(df_solo)
+                    
+                df_solo = df_solo.where(pd.notnull(df_solo), None)
+                df_variant = df_variant.where(pd.notnull(df_variant), None)
+                final_list = df_variant.to_dict("records") + df_solo.to_dict("records")
+                return final_list
+    
+            # If include_variants is TRUE, then PARENT, VARIANT and SOLO will be sent and PARENT wont have VARIANT in it
+            if include_variants:
+                if exclude_pim_properties:
+                    df = self.exclude_pim_properties(df)
+                    
+                df = df.where(pd.notnull(df), None)
+                final_list = df.to_dict('records')
+                return final_list
+    
+            
+            
+            # Separate parent, variant, and solo products
+            df_parent = df[df['pimProductType'] == 'PARENT']
             df_variant = df[df['pimProductType'] == 'VARIANT']
             df_solo = df[df['pimProductType'] == 'SOLO']
-            
-            if exclude_pim_properties:
-                df_variant = self.exclude_pim_properties(df_variant)
-                df_solo = self.exclude_pim_properties(df_solo)
-                
-            df_solo = df_solo.where(pd.notnull(df_solo), None)
-            df_variant = df_variant.where(pd.notnull(df_variant), None)
-            final_list = df_variant.to_dict("records") + df_solo.to_dict("records")
-            return final_list
-
-        # If include_variants is TRUE, then PARENT, VARIANT and SOLO will be sent and PARENT wont have VARIANT in it
-        if include_variants:
-            if exclude_pim_properties:
-                df = self.exclude_pim_properties(df)
-                
-            df = df.where(pd.notnull(df), None)
-            final_list = df.to_dict('records')
-            return final_list
-
-        
-        
-        # Separate parent, variant, and solo products
-        df_parent = df[df['pimProductType'] == 'PARENT']
-        df_variant = df[df['pimProductType'] == 'VARIANT']
-        df_solo = df[df['pimProductType'] == 'SOLO']
-
-        # Remove Nan
-        df_parent = df_parent.where(pd.notnull(df_parent), None)
-        df_variant = df_variant.where(pd.notnull(df_variant), None)
-        df_solo = df_solo.where(pd.notnull(df_solo), None)
-
-        
-        # If include_variants are FALSE, group_by_parent is TRUE and  exclude_pim_properties is TRUE
-        # then, the variant product inside the parent product wouls still have the pim properties
-        # please handle this inside the job code
-        # in this case, PRENT products will have their VARIANTS inside them in "variants" column. This will also include SOLO products
-        
-        grouped_variants = df_variant.groupby('pimParentId').apply(lambda x: x.to_dict('records')).reset_index()
-        grouped_variants.columns = ['pimUniqueId', 'variants']
-
-        merged_df = pd.merge(df_parent, grouped_variants, on='pimUniqueId', how='left')
-
-        merged_df['variants'] = merged_df['variants'].apply(lambda x: x if isinstance(x, list) else [])
     
-        if exclude_pim_properties:
-            merged_df = self.exclude_pim_properties(merged_df)
-            df_solo = self.exclude_pim_properties(df_solo)                
+            # Remove Nan
+            df_parent = df_parent.where(pd.notnull(df_parent), None)
+            df_variant = df_variant.where(pd.notnull(df_variant), None)
+            df_solo = df_solo.where(pd.notnull(df_solo), None)
+    
+            
+            # If include_variants are FALSE, group_by_parent is TRUE and  exclude_pim_properties is TRUE
+            # then, the variant product inside the parent product wouls still have the pim properties
+            # please handle this inside the job code
+            # in this case, PRENT products will have their VARIANTS inside them in "variants" column. This will also include SOLO products
+            if not df_variant.empty:
+                grouped_variants = df_variant.groupby('pimParentId').apply(lambda x: x.to_dict('records')).reset_index()
+                grouped_variants.columns = ['pimUniqueId', 'variants']
         
-        parent_list = merged_df.to_dict('records')
-        final_list = parent_list + df_solo.to_dict('records')
-
-        return final_list
+                merged_df = pd.merge(df_parent, grouped_variants, on='pimUniqueId', how='left')
+        
+                merged_df['variants'] = merged_df['variants'].apply(lambda x: x if isinstance(x, list) else [])
+            else:
+                merged_df = pd.DataFrame()
+        
+            if exclude_pim_properties:
+                merged_df = self.exclude_pim_properties(merged_df)
+                df_solo = self.exclude_pim_properties(df_solo)                
+            
+            parent_list = merged_df.to_dict('records')
+            final_list = parent_list + df_solo.to_dict('records')
+    
+            return final_list
+        except Exception as e:
+            print(e)
+            print_exc()
+            return []
 
     def process_and_send_errors(self, df):
-        for index, row in df.iterrows():
-            product_id = row.get('pimUniqueId', "")
-
-            error_messages = []
-            if 'pimValidationErrors' in row and pd.notna(row['pimValidationErrors']):
-                error_messages.append(str(row['pimValidationErrors']))
-
-            if 'pimPostTransformerErrors' in row and pd.notna(row['pimPostTransformerErrors']):
-                error_messages.append(str(row['pimValidationErrors']))
-
-            if 'pimTransformationErrors' in row and pd.notna(row['pimTransformationErrors']):
-                error_messages.append(str(row['pimValidationErrors']))
-
-            full_error_message = '|'.join(error_messages)
-
-            self.insert_product_status(pid=product_id, status="FAILED",
-                                       status_desc=full_error_message)
-
-        return df.to_dict("records")
+        try:
+            for index, row in df.iterrows():
+                product_id = row.get('pimUniqueId', "")
+    
+                error_messages = []
+                if 'pimValidationErrors' in row and pd.notna(row['pimValidationErrors']):
+                    error_messages.append(str(row['pimValidationErrors']))
+    
+                if 'pimPostTransformerErrors' in row and pd.notna(row['pimPostTransformerErrors']):
+                    error_messages.append(str(row['pimValidationErrors']))
+    
+                if 'pimTransformationErrors' in row and pd.notna(row['pimTransformationErrors']):
+                    error_messages.append(str(row['pimValidationErrors']))
+    
+                full_error_message = '|'.join(error_messages)
+    
+                self.insert_product_status(pid=product_id, status="FAILED",
+                                           status_desc=full_error_message)
+    
+            return df.to_dict("records")
+        except Exception as e:
+            print(e)
+            print_exc()
+            return []
 
     def fetch_all_pim_products(self, include_variants=False, exclude_pim_properties=False):
         raw_products_list = []
